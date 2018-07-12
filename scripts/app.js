7e70eb49eff5ee40d9b538b2b09f1ff3a5a58bc0
mfaibrahem <mfaibrahem2013@gmail.com>
2018-07-12 18:26:21 +0200

    remove all tasks

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,8 @@ const UItask = document.querySelector('#task');
 const UItaskList = document.querySelector('.collection');
 const UIclearBtn = document.querySelector('.clear-tasks');
 const UIfilter = document.querySelector('#filter');
-const UIgreenAlert = document.querySelector('.green-alert');
+const UIredAlert = document.querySelector('.alert-red');
+const UIgreenAlert = document.querySelector('.alert-green');
 
 loadEventListeners();
 
@@ -14,12 +15,14 @@ function loadEventListeners() {
   UIform.addEventListener('submit', addTask);
   // Remove Task Event 
   UItaskList.addEventListener('click', removeTask);
+  // Remove all Tasks
+  UIclearBtn.addEventListener('click', clearTasks);
 }
 
 // addTask function
 function addTask(e) {
   if (UItask.value === '' || parseInt(UItask.value) < 0) {
-    timeFun(UIgreenAlert, 'Please Enter A Valid Task', 'tomato', 3000);
+    timeFun(UIredAlert, 'Please Enter A Valid Task', 'tomato', 3000);
   } else {
     const li = document.createElement('li');
     li.classList.add('created-list');
@@ -47,12 +50,23 @@ function removeTask(e) {
   }
 }
 
+function clearTasks() {
+  if (UItaskList.innerHTML === '') {
+    timeFun(UIgreenAlert, 'Please enter task first', 'teal', 3000);
+  } else {
+    if (confirm('Are You Sure!')) {
+      UItaskList.innerHTML = '';
+    }
+  }
+
+}
+
 
 function timeFun(UIelement, message, color, time) {
   UIelement.innerText = message;
   UIelement.style.backgroundColor = color;
-  UIelement.classList.add('green-alert-on');
+  UIelement.classList.add('alert-on');
   setTimeout(() => {
-    UIelement.classList.remove('green-alert-on');
+    UIelement.classList.remove('alert-on');
   }, time);
 }
